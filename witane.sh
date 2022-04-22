d7d7ef638548a05b36918f4d577e6b4833c23d86
d4kox <101132138+d4kox@users.noreply.github.com>
2022-04-22 13:50:38 -0300

    Update witane.sh

--- witane.sh
+++ witane.sh
@@ -6,11 +6,20 @@ function start_setup() {
     echo -e "[*] Info : ruta ej. /media/$usuario/ESD-USB/comandos/"
     echo -e "Ruta absoluta de sus comandos: "; read CommandsLocation
     cp $CommandsLocation* /usr/bin/
-    chmod +x /usr/bin/*    
+    chmod +x /usr/bin/*
+    echo -e "Quieres instalar el dev-packet (paquete de desarrollador)?? [y/n]"; read devPctI
+    if [ $devPctI == "y" ]
+    then
+        sudo apt install code terminator
+    elif [ $devPctI == "n" ]
+        echo -e "[*] Entendido... Cancelando la instalacion de programas..."
+    else
+        echo -e "Err: Opcion no existe"
 }
 if [ $usuario != 'root' ]
 then
     echo -e "[!] Debes ejecutar Witane como super-usuario(root)"
 else
     start_setup
+    echo -e "[*] Witane ha instalado todo correctamente"
 fi
